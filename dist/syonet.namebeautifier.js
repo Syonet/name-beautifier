cdd8ac96ca5c8b9ecfe401dd22cc0ff55857808d
Gustavo Henke <gustavo@injoin.io>
2014-12-18 14:37:26 -0200

    chore(release): version 0.1.0

---
+++ dist/syonet.namebeautifier.js
@@ -0,0 +1,52 @@
+!function () {
+    "use strict";
+
+    angular.module( "syonet.namebeautifier", [] ).provider( "$nameBeautifier", function () {
+        var provider = this;
+
+        provider.skippedWords = [
+            "de",
+            /^d(o|a)s?$/
+        ];
+
+        provider.$get = function () {
+            var api = {};
+            api.beautify = function ( input ) {
+                if ( typeof input !== "string" ) {
+                    return input;
+                }
+
+                input = input.split( /\s+/ ).map(function ( part ) {
+                    part = part.toLowerCase();
+
+                    if ( !provider.skippedWords.some( testSkippedWord, part ) ) {
+                        part = part[ 0 ].toUpperCase() + part.substr( 1 );
+                    }
+
+                    return part;
+                }).join( " " );
+
+                return input;
+            };
+
+            return api;
+        };
+
+        return provider;
+
+        // -----------------------------------------------------------------------------------------
+
+        function testSkippedWord ( word ) {
+            var str =  this.toString();
+            if ( typeof word === "string" ) {
+                return str === word;
+            }
+
+            return word.test( str );
+        }
+    }).filter( "beautifyName", ["$nameBeautifier", function ( $nameBeautifier ) {
+        return function ( input ) {
+            return $nameBeautifier.beautify( input );
+        };
+    }]);
+}();
