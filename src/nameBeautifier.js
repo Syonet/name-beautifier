7713f45fc76de1c9b8ca88c4e1ba392a47ea52e2
Gustavo Henke <gustavo@injoin.io>
2014-12-19 09:51:18 -0200

    docs(nameBeautifier): apply JSDocs

--- src/nameBeautifier.js
+++ src/nameBeautifier.js
@@ -4,11 +4,19 @@
     angular.module( "syonet.namebeautifier", [] ).provider( "$nameBeautifier", function () {
         var provider = this;
 
+        /**
+         * Words that should be skipped when formatting
+         * @type    {String[]}
+         */
         provider.skippedWords = [
             "de",
             /^d(o|a)s?$/
         ];
 
+        /**
+         * Words that should be uppercased when formatting
+         * @type    {String[]}
+         */
         provider.uppercaseWords = [
             /^s(\/|\.)?a\.?$/
         ];
@@ -16,11 +24,14 @@
         provider.$get = function () {
             var api = {};
             api.beautify = function ( input ) {
+                // Quick fail for non string inputs
                 if ( typeof input !== "string" ) {
                     return input;
                 }
 
                 input = input.trim().split( /\s+/ ).map(function ( part ) {
+                    // If this part is empty, we'll skip it, it won't make a difference if
+                    // we format it or not
                     if ( !part ) {
                         return part;
                     }
@@ -46,6 +57,13 @@
 
         // -----------------------------------------------------------------------------------------
 
+        /**
+         * Test a word expression against the function context.
+         * This function is proper for usage as an array method callback (like .some()/.all())
+         *
+         * @param   {String|RegExp} word
+         * @returns {Boolean}
+         */
         function testWord ( word ) {
             var str = this.toString();
             if ( typeof word === "string" ) {
